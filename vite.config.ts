cc3ff3c60fd288b2e26b0f07a0d54c4047d138af
Bel Borba <isaborba.cs@gmail.com>
2025-09-01 19:52:14 -0300

    chore: adjust query

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/pokedex-react-graphql-apollo/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
